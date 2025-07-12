d3c8f532f003265cadf38bc2548bdd96073a2d5d
mads.emil.langstad@kameleongruppen.no <Mads-Kameleon>
2025-07-12 16:20:37 +0200

    configured vite for multi-entry build (background, content, scripts, popup)

--- src/content/youtube.ts
+++ src/content/youtube.ts
@@ -1,4 +1,5 @@
 import { FilterSettings, VideoMetadata } from '../types';
+import { injectStyles } from './youtube-styles';
 
 class YouTubeFilter {
   private settings: FilterSettings = {
@@ -13,6 +14,7 @@ class YouTubeFilter {
   }
 
   private async init() {
+    injectStyles();
     await this.loadSettings();
     this.setupMessageListeners();
     this.injectUI();
